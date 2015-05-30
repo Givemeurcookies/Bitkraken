90792a4129c009f63ce29b87b21188ce73dfc173
Jon Brede Skaug <jbs@ramenstudios.com>
2015-05-30 22:25:17 +0200

    Half working tile animation with random colour palettes

--- devbuild/js/canvasAnimation.js
+++ devbuild/js/canvasAnimation.js
@@ -1,6 +1,7 @@
-var canvas  = document.getElementById('headCanvas'),
-    hdc     = canvas.getContext('2d'),
-    canvasMeta = {};
+var canvas     = document.getElementById('headCanvas'),
+    hdc        = canvas.getContext('2d'),
+    canvasMeta = {},
+    helloInt   = [];
 
 Init();
 
@@ -11,8 +12,33 @@ function Init()
     fullSizeCanvas();
 
     // Fill the path
-    hdc.fillStyle = "red";
-    hdc.fillRect(0,0,canvasMeta.height/100*5,canvasMeta.height/100*5);
+    var gt = makeGlobalTimer(100);
+    for(var i = 0; i < 60; i++){
+        helloInt[i] = 0;
+        gt.registerCallback(function () {
+
+            if(this.delay < 0){
+                for(var i = 0; i < 6; i++){
+                    drawSquare({
+                        "x" : this.y,
+                        "y" : this.x-i
+                    }, 
+                    1-(0.20*i),
+                    this.color);
+                }
+
+                this.x++;
+            } else {
+                this.delay--;
+            }
+        
+        }.bind({
+            "y"     : i,
+            "x"     : 0,
+            "delay" : getRandomInt(0, 20),
+            "color" : generateRandomColor([35,175,250])}
+        ));
+    }
 }
 
 
@@ -37,4 +63,74 @@ function fullSizeCanvas() {
     canvas.style.width  = canvasMeta.width;
     canvas.style.height = canvasMeta.height;
     hdc.scale(pixelRatio, pixelRatio);
+}
+function drawSquare(pos, opacity, rgb){
+    var size = Math.floor(canvasMeta.height/100*5);
+        
+
+    hdc.clearRect(size*pos.x,
+                 size*pos.y,
+                 size,
+                 size);
+    hdc.globalAlpha=opacity;
+    hdc.fillStyle = 'rgb(' + rgb.red + ',' +
+                    rgb.green + ','+rgb.blue+')';
+    hdc.fillRect(size*pos.x,
+                 size*pos.y,
+                 size,
+                 size);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+// Thanks to:
+// http://stackoverflow.com/a/2699792/1327054
+function makeGlobalTimer(freq) {
+  freq = freq || 1000;
+
+  // array of callback functions
+  var callbacks = [];
+
+  // register the global timer
+  var id = setInterval(
+    function() {
+      var idx;
+      for (idx in callbacks) {
+        callbacks[idx]();
+      }
+    }, freq);
+
+  // return a Global Timer object
+  return {
+    "id": function() { return id; },
+    "registerCallback": function(cb) {
+      callbacks.push(cb);
+    },
+    "cancel": function() {
+      if (id !== null) {
+        clearInterval(id);
+        id = null;
+      }
+    }
+  };
+}
+
+function generateRandomColor(mix) {
+
+    var red   = getRandomInt(0, 256),
+        green = getRandomInt(0, 256),
+        blue =  getRandomInt(0, 256);
+
+    // mix the color
+    if (mix != null) {
+        red = (red + mix[0]) / 2;
+        green = (green + mix[1]) / 2;
+        blue = (blue + mix[2]) / 2;
+    }
+
+    rgb = {"red" : red, "green" : green, "blue" : blue};
+    return rgb;
 }
